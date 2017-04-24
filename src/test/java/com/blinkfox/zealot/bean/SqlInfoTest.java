dce5dab1a86a76e42bb65bcd2b28266f578530b8
blinkfox <chenjiayin1990@163.com>
2017-04-24 23:29:04 +0800

    新增了SqlInfoTest的测试类

---
+++ src/test/java/com/blinkfox/zealot/bean/SqlInfoTest.java
@@ -0,0 +1,44 @@
+package com.blinkfox.zealot.bean;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+/**
+ * SqlInfo的单元测试类.
+ * Created by blinkfox on 2017/4/24.
+ */
+public class SqlInfoTest {
+
+    private static SqlInfo sqlInfo = null;
+
+    /**
+     * 初始化方法.
+     */
+    @BeforeClass
+    public static void init() {
+        sqlInfo = SqlInfo.newInstance();
+    }
+
+    /**
+     * 测试生成实例的方法.
+     */
+    @Test
+    public void testNewInstance() {
+        assertEquals(0, sqlInfo.getJoin().length());
+        assertArrayEquals(new Object[]{}, sqlInfo.getParamsArr());
+    }
+
+    /**
+     * 测试获取参数并转换成数组的方法.
+     */
+    @Test
+    public void testGetParamsArr() {
+        sqlInfo.setParams(null);
+        assertArrayEquals(new Object[]{}, sqlInfo.getParamsArr());
+    }
+
+}
