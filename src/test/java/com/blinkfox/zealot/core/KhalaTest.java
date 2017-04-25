e7517644c0eb5675433bd0290e2f5997f9620e77
blinkfox <chenjiayin1990@163.com>
2017-04-25 23:31:34 +0800

    完善KhalaTest中的单元测试方法

--- src/test/java/com/blinkfox/zealot/core/KhalaTest.java
+++ src/test/java/com/blinkfox/zealot/core/KhalaTest.java
@@ -32,4 +32,80 @@ public class KhalaTest {
         assertEquals(expectedSql, sql);
     }
 
+    /**
+     * 测试end()方法.
+     */
+    @Test
+    public void testEnd() {
+        String oldSql = "   SELECT name, email FROM user \n WHERE id = '2'   ";
+        String sql = Khala.newInstance().start().add(oldSql).end();
+        assertEquals("SELECT name, email FROM user WHERE id = '2'", sql);
+    }
+
+    /**
+     * 测试select生成sql的方法.
+     */
+    @Test
+    public void testSelect() {
+        String sql = Khala.newInstance().start()
+                .select("*")
+                .from("user as u")
+                .innerJoin("user_detail as ud").on("ud.id = u.id")
+                .rightJoin("dept").as("d").on("u.dept_id = d.id")
+                .fullJoin("corp").as("c").on("u.corp_id = c.id")
+                .where("u.age > ?")
+                .groupBy("u.dept")
+                .having("u.money > ?")
+                .orderBy("u.age DESC")
+                .end();
+
+        String expectedSql = "SELECT * FROM user as u INNER JOIN user_detail as ud ON ud.id = u.id "
+                + "RIGHT JOIN dept AS d ON u.dept_id = d.id FULL JOIN corp AS c ON u.corp_id = c.id "
+                + "WHERE u.age > ? GROUP BY u.dept HAVING u.money > ? ORDER BY u.age DESC";
+        assertEquals(expectedSql, sql);
+    }
+
+    /**
+     * 测试insert语句生成sql的方法.
+     */
+    @Test
+    public void testInsert() {
+        String sql = Khala.newInstance().start()
+                .insertInto("user(name, email)")
+                .values("(?, ?)")
+                .end();
+
+        String expectedSql = "INSERT INTO user(name, email) VALUES (?, ?)";
+        assertEquals(expectedSql, sql);
+    }
+
+    /**
+     * 测试update语句生成sql的方法.
+     */
+    @Test
+    public void testUpdate() {
+        String sql = Khala.newInstance().start()
+                .update("user")
+                .set("name = ?")
+                .where("id = ?")
+                .end();
+
+        String expectedSql = "UPDATE user SET name = ? WHERE id = ?";
+        assertEquals(expectedSql, sql);
+    }
+
+    /**
+     * 测试update语句生成sql的方法.
+     */
+    @Test
+    public void testDelete() {
+        String sql = Khala.newInstance().start()
+                .delete("FROM user")
+                .where("id = ?")
+                .end();
+
+        String expectedSql = "DELETE FROM user WHERE id = ?";
+        assertEquals(expectedSql, sql);
+    }
+
 }
