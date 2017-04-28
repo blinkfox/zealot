bb99cf0fae440058e86c9f7bcfd86d47bbfa4911
blinkfox <chenjiayin1990@163.com>
2017-04-29 01:12:05 +0800

    新增SqlInfoBuilder的单元测试

---
+++ src/test/java/com/blinkfox/zealot/core/builder/SqlInfoBuilderTest.java
@@ -0,0 +1,56 @@
+package com.blinkfox.zealot.core.builder;
+
+import com.blinkfox.zealot.bean.BuildSource;
+import com.blinkfox.zealot.bean.SqlInfo;
+import com.blinkfox.zealot.log.Log;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * SqlInfoBuilder单元测试类.
+ * Created by blinkfox on 2017/4/29.
+ */
+public class SqlInfoBuilderTest {
+
+    private static final Log log = Log.get(SqlInfoBuilderTest.class);
+
+    /**
+     * 构建区间查询的测试方法.
+     */
+    @Test
+    public void testBuildBetweenSql() {
+        BuildSource source = new BuildSource(SqlInfo.newInstance()).setPrefix("");
+        SqlInfo sqlInfo = SqlInfoBuilder.newInstace(source)
+                .buildBetweenSql("u.age", null, 20);
+
+        Assert.assertEquals("u.age <= ? ", sqlInfo.getJoin().toString());
+        Assert.assertArrayEquals(new Object[]{20}, sqlInfo.getParamsArr());
+    }
+
+    /**
+     * 构建区间查询的测试方法2.
+     */
+    @Test
+    public void testBuildBetweenSql2() {
+        BuildSource source = new BuildSource(SqlInfo.newInstance()).setPrefix("");
+        SqlInfo sqlInfo = SqlInfoBuilder.newInstace(source)
+                .buildBetweenSql("u.age", 15, null);
+
+        Assert.assertEquals("u.age >= ? ", sqlInfo.getJoin().toString());
+        Assert.assertArrayEquals(new Object[]{15}, sqlInfo.getParamsArr());
+    }
+
+    /**
+     * 构建区间查询的测试方法2.
+     */
+    @Test
+    public void testBuildInSql() {
+        BuildSource source = new BuildSource(SqlInfo.newInstance()).setPrefix("");
+        SqlInfo sqlInfo = SqlInfoBuilder.newInstace(source)
+                .buildInSql("u.sex", null);
+
+        Assert.assertEquals("", sqlInfo.getJoin().toString());
+        Assert.assertArrayEquals(new Object[]{}, sqlInfo.getParamsArr());
+    }
+
+}
