99295a22fa858face4346229b658bdbf199ed14f
blinkfox <chenjiayin1990@163.com>
2017-05-05 15:19:18 +0800

    删除banner文件，改用字符串替代，读取jar包中的文件出错，修改版本为1.1.4-SNAPSHOT

--- src/main/java/com/blinkfox/zealot/loader/BannerLoader.java
+++ src/main/java/com/blinkfox/zealot/loader/BannerLoader.java
@@ -1,18 +1,21 @@
 package com.blinkfox.zealot.loader;
 
-import com.blinkfox.zealot.helpers.IoHelper;
 import com.blinkfox.zealot.log.Log;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.Reader;
-import java.net.URL;
 
 /**
  * 加载zealot banner.txt文件的加载器类.
  * Created by blinkfox on 2017/4/21.
  */
 public class BannerLoader {
+
+    /* banner文本 */
+    private static final String bannerText = "\n"
+            + "__________               .__           __\n"
+            + "\\____    / ____  _____   |  |    _____/  |_\n"
+            + "  /     /_/ __ \\ \\__  \\  |  |   /  _ \\   __\\\n"
+            + " /     /_\\  ___/  / __ \\_|  |__(  <_> )  |\n"
+            + "/_______ \\\\___  >(____  /|____/ \\____/|__|\n"
+            + "        \\/    \\/      \\/\n";
 
     private static final Log log = Log.get(BannerLoader.class);
 
@@ -35,36 +38,7 @@ public class BannerLoader {
      * 打印banner文件中的内容.
      */
     public void print() {
-        URL banner = this.getClass().getResource("/banner.txt");
-        if (banner != null) {
-            log.info(this.txtToStr(new File(banner.getPath())));
-        }
-    }
-
-    /**
-     * 获取txt文件中的内容并转换为字符串.
-     * @param file 文件
-     * @return 字符串
-     */
-    private String txtToStr(File file) {
-        StringBuilder sb = new StringBuilder("");
-        Reader fileReader = null;
-        try {
-            fileReader = new FileReader(file);
-            BufferedReader bufferReader = new BufferedReader(fileReader);
-
-            // 一行一行的读取字符串，并将结果追加到StringBuilder中
-            String line;
-            while ((line = bufferReader.readLine()) != null) {
-                sb.append(System.getProperty("line.separator")).append(line);
-            }
-        } catch (Exception e) {
-            log.error("打印zealot banner失败！", e);
-        } finally {
-            IoHelper.closeQuietly(fileReader);
-        }
-
-        return sb.toString();
+        log.info(bannerText);
     }
 
 }
